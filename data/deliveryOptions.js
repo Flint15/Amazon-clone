700047d3861b6420e42f57c1e59f8dd09ff7826f
IrIs <chesspace341@gmail.com>
2025-11-29 14:19:37 +0100

    Moved delivery date calculation into deliveryOptions.js

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,3 +1,5 @@
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
+
 export function getDeliveryOption(deliveryOptionId) {
   let deliveryOption
 
@@ -8,6 +10,19 @@ export function getDeliveryOption(deliveryOptionId) {
   })
 
   return deliveryOption || deliveryOptions[0]
+}
+
+export function calculateDeliveryDate(deliveryOption) {
+  const today = dayjs()
+  const deliveryDate = today.add(
+    deliveryOption.deliveryDays,
+    'days'
+  )
+  const dateString = deliveryDate.format(
+    'dddd, MMMM D'
+  )
+
+  return dateString
 }
 
 export const deliveryOptions = [{
