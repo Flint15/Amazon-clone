988b88932cfb16e877fba6b916709a0b36413eb1
IrIs <chesspace341@gmail.com>
2025-11-29 11:34:05 +0100

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,8 @@ import {
   calculateCartQuantity, 
   cart, 
   removeFromCart, 
-  updateQuantity
+  updateQuantity,
+  updateDeliveryOption
 } from '../data/cart.js'
 import {products} from '../data/products.js'
 import {formatCurrency} from './utils/money.js'
@@ -118,7 +119,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId
 
       html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -137,8 +140,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
   return html
 }
 
+
 document.querySelector('.js-order-summary')
-  .innerHTML = cartSummaryHTML
+.innerHTML = cartSummaryHTML
 
 document.querySelectorAll('.js-delete-link')
   .forEach(link => {
@@ -146,7 +150,7 @@ document.querySelectorAll('.js-delete-link')
       const productId = link.dataset.productId
       removeFromCart(productId)
       updateCartQuantity()
-
+      
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`
       )
@@ -154,6 +158,14 @@ document.querySelectorAll('.js-delete-link')
     })
   })
 
+document.querySelectorAll('.js-delivery-option')
+  .forEach(element => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset
+      updateDeliveryOption(productId, deliveryOptionId)
+    })
+  })
+  
 function updateCartQuantity() {
   const cartQuantity = calculateCartQuantity()
   
