baec39d26539c8bfaf55c40405d576662c9fcb75
IrIs <chesspace341@gmail.com>
2025-12-02 16:20:21 +0100

    update delivery option in cart (Exercise 16 lesson)

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage, removeFromCart} from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart, updateDeliveryOption} from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   beforeEach(() => {
@@ -88,5 +88,56 @@ describe('test suite: removeFromCart', () => {
       quantity: 1,
       deliveryOptionId: '1'
     }]));
+  })
+})
+
+describe('test suite: updateDeliveryOption', () => {
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
+  
+  it('Update the delivey option of the product in the cart', () => {
+    spyOn(localStorage, 'setItem')
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+        return JSON.stringify([{
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: '1'
+        }, {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: '2'
+        }])
+      })
+    loadFromStorage()
+    
+    expect(cart[0].deliveryOptionId).toEqual('1')
+    updateDeliveryOption(productId1, '2')
+    expect(cart[0].deliveryOptionId).toEqual('2')
+
+    expect(cart).toEqual(
+      [{
+        productId: productId1,
+        quantity: 2,
+        deliveryOptionId: '2'
+      }, {
+        productId: productId2,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }]
+    )
+
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+    expect(localStorage.setItem)
+      .toHaveBeenCalledWith('cart', JSON.stringify(
+        [{
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: '2'
+        }, {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: '2'
+        }]
+      ))
   })
 })
