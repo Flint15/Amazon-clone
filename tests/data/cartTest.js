1b549bbda7eb397ea17ecd61df2754f4e6249f98
IrIs <chesspace341@gmail.com>
2025-12-02 16:36:53 +0100

    test for edge case when product isn't in the cart

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -95,7 +95,7 @@ describe('test suite: updateDeliveryOption', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
   
-  it('Update the delivey option of the product in the cart', () => {
+  beforeEach(() => {
     spyOn(localStorage, 'setItem')
     spyOn(localStorage, 'getItem').and.callFake(() => {
         return JSON.stringify([{
@@ -109,7 +109,9 @@ describe('test suite: updateDeliveryOption', () => {
         }])
       })
     loadFromStorage()
-    
+  })
+
+  it('Update the delivey option of the product in the cart', () => {
     expect(cart[0].deliveryOptionId).toEqual('1')
     updateDeliveryOption(productId1, '2')
     expect(cart[0].deliveryOptionId).toEqual('2')
@@ -139,5 +141,20 @@ describe('test suite: updateDeliveryOption', () => {
           deliveryOptionId: '2'
         }]
       ))
+  })
+  it('Update the delivey option of the product that isn\'t in the cart', () => {
+    updateDeliveryOption('123')
+    expect(cart).toEqual(
+      [{
+        productId: productId1,
+        quantity: 2,
+        deliveryOptionId: '1'
+      }, {
+        productId: productId2,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }]
+    )
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0)
   })
 })
