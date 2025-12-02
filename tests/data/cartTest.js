7b1e3903fc5ee745d3e5ae6cfdf2786f28506eda
IrIs <chesspace341@gmail.com>
2025-12-02 15:07:41 +0100

    test for the removeFromCart function (Exercise 16 lesson)

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart} from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   beforeEach(() => {
@@ -52,5 +52,41 @@ describe('test suite: addToCart', () => {
         deliveryOptionId: '1'
         }]
       ))
+  })
+})
+
+describe('test suite: removeFromCart', () => {
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
+  
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem')
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }])
+    })
+    loadFromStorage()
+  })
+
+  it('remove a productId that is in the cart', () => {    
+    removeFromCart(productId1)
+    expect(cart.length).toEqual(0)
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]));
+  })
+  it('removes a productId that is not in the cart', () => {
+    removeFromCart(productId2)
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '1'
+    }]));
   })
 })
