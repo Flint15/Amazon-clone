2a1a00b9c86d4246c18a6e639bc3909c23ce425e
IrIs <chesspace341@gmail.com>
2025-12-05 00:58:24 +0100

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -105,6 +105,35 @@ const object3 = {
   }
 }*/
 
+export let products = []
+
+export function loadProducts(func) {
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response)
+      .map((productDetails) => {
+        if (productDetails.type === 'clothing') {
+          return new Clothing(productDetails)
+        }
+
+        if (productDetails.type === 'appliance') {
+          return new Appliance(productDetails)
+        }
+
+        return new Product(productDetails)
+      })
+  
+    console.log('load products')
+
+    func()
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products')
+  xhr.send()
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -787,3 +816,4 @@ export const products = [
 
   return new Product(productDetails)
 })
+*/
